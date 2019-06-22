1202e7ff06b082ee6d5044cc323d773588ff8543
Mason Short <ironicallyshort737@hotmail.com>
2019-06-22 11:45:07 -0600

    template updated

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyCfu5kEk-9UxRv2-F3Qa6hELFINV1GUPVM",
+    authDomain: "lernding.firebaseapp.com",
+    databaseURL: "https://lernding.firebaseio.com",
+    projectId: "lernding",
+    storageBucket: "lernding.appspot.com",
+    messagingSenderId: "20200720094",
+    appId: "1:20200720094:web:7fc7a41473127f9c"
+  };
+firebase.initializeApp(firebaseConfig);
+
+let database = firebase.database();
+
+database.ref('wargame/users').set('');
+// TODO push new users to firebase (wargame/users)
